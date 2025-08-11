7dc0c98f166e28a330262bc51766817c88a1b1cf
donvovo2 <38768072+donvovo2@users.noreply.github.com>
2025-08-10 22:23:15 -0400

    new start

--- lib/dummy_web_video_player.dart
+++ lib/dummy_web_video_player.dart
@@ -1,6 +1,6 @@
-// Dummy implementation for non-web platforms
-class WebVideoPlayer {
-  static void registerIframe(String iframeId, String youtubeVideoId) {
-    // Do nothing on non-web platforms
-  }
-}
+// // Dummy implementation for non-web platforms
+// class WebVideoPlayer {
+//   static void registerIframe(String iframeId, String youtubeVideoId) {
+//     // Do nothing on non-web platforms
+//   }
+// }
--- lib/web_video_player.dart
+++ lib/web_video_player.dart
@@ -1,18 +1,18 @@
-import 'dart:html' as html;
-import 'dart:ui_web' as ui_web;
+// import 'dart:html' as html;
+// import 'dart:ui_web' as ui_web;
 
-class WebVideoPlayer {
-  static void registerIframe(String iframeId, String youtubeVideoId) {
-    final String videoUrl =
-        'https://www.youtube.com/embed/$youtubeVideoId?autoplay=1&rel=0';
-    ui_web.platformViewRegistry.registerViewFactory(
-      iframeId,
-      (int viewId) => html.IFrameElement()
-        ..src = videoUrl
-        ..style.border = 'none'
-        ..style.width = '100%'
-        ..style.height = '100%'
-        ..allowFullscreen = true,
-    );
-  }
-}
+// class WebVideoPlayer {
+//   static void registerIframe(String iframeId, String youtubeVideoId) {
+//     final String videoUrl =
+//         'https://www.youtube.com/embed/$youtubeVideoId?autoplay=1&rel=0';
+//     ui_web.platformViewRegistry.registerViewFactory(
+//       iframeId,
+//       (int viewId) => html.IFrameElement()
+//         ..src = videoUrl
+//         ..style.border = 'none'
+//         ..style.width = '100%'
+//         ..style.height = '100%'
+//         ..allowFullscreen = true,
+//     );
+//   }
+// }
